609e9413418a6422382a53ded51ee25e03ae8e5b
Alban Sagouis <alban.sagouis@idiv.de>
2022-07-12 14:09:04 +0200

    wrangling lightfoot_2022

--- R/data download/lightfoot_2022.R
+++ R/data download/lightfoot_2022.R
@@ -2,8 +2,8 @@
 
 dataset_id <- "lightfoot_2022"
 if (!file.exists("./data/raw data/lightfoot_2022/lizard_pitfall_data_89-06.rds")) {
-  download.file(url = "https://pasta.lternet.edu/package/data/eml/knb-lter-jrn/210007001/38/731f52d77045dfc5957589d35c2e6227",destfile = "./data/raw data/lightfoot_2022/lizard_pitfall_data_89-06.csv", mode = "wb")
-  
+  download.file(url = "https://pasta.lternet.edu/package/data/eml/knb-lter-jrn/210007001/38/731f52d77045dfc5957589d35c2e6227",destfile = "./data/cache/lightfoot_2022/lizard_pitfall_data_89-06.csv", mode = "wb")
+
   ddata <- read.csv(
       file = "./data/cache/lightfoot_2022/lizard_pitfall_data_89-06.csv"
   )
--- R/data wrangling/lightfoot_2022.R
+++ R/data wrangling/lightfoot_2022.R
@@ -1,113 +1,114 @@
 ## lightfoot_2022
+dataset_id <- "lightfoot_2022"
 library(ggplot2)
 library(dplyr)
-ddata<- readRDS("data/raw data/lightfoot_2022/lizard_pitfall_data_89-06.rds")
+ddata <- base::readRDS("data/raw data/lightfoot_2022/lizard_pitfall_data_89-06.rds")
 #data extraction
 # Fix any interval or ratio columns mistakenly read in as nominal and nominal columns read as numeric or dates read as strings
-dataset_id <- "lightfoot_2022"
-# attempting to convert ddata$date dateTime string to R date structure (date or POSIXct)                                
+# attempting to convert ddata$date dateTime string to R date structure (date or POSIXct)
 tmpDateFormat<-"%Y-%m-%d"
 tmp1date<-as.Date(ddata$date,format=tmpDateFormat)
 # Keep the new dates only if they all converted correctly
-if(length(tmp1date) == length(tmp1date[!is.na(tmp1date)])){ddata$date <- tmp1date } else {print("Date conversion failed for ddata$date. Please inspect the data and do the date conversion yourself.")}                                                                    
-rm(tmpDateFormat,tmp1date) 
+if(length(tmp1date) == length(tmp1date[!is.na(tmp1date)])){ddata$date <- tmp1date } else {print("Date conversion failed for ddata$date. Please inspect the data and do the date conversion yourself.")}
+rm(tmpDateFormat,tmp1date)
 if (class(ddata$zone)!="factor") ddata$zone<- as.factor(ddata$zone)
 if (class(ddata$site)!="factor") ddata$site<- as.factor(ddata$site)
 if (class(ddata$plot)!="factor") ddata$plot<- as.factor(ddata$plot)
-if (class(ddata$pit)=="factor") ddata$pit <-as.numeric(levels(ddata$pit))[as.integer(ddata$pit) ]               
+if (class(ddata$pit)=="factor") ddata$pit <-as.numeric(levels(ddata$pit))[as.integer(ddata$pit) ]
 if (class(ddata$pit)=="character") ddata$pit <-as.numeric(ddata$pit)
 if (class(ddata$spp)!="factor") ddata$spp<- as.factor(ddata$spp)
 
 # Convert Missing Values to NA for non-dates
 
 ddata$plot <- as.factor(ifelse((trimws(as.character(ddata$plot))==trimws("NA")),NA,as.character(ddata$plot)))
-ddata$pit <- ifelse((trimws(as.character(ddata$pit))==trimws("NA")),NA,ddata$pit)               
+ddata$pit <- ifelse((trimws(as.character(ddata$pit))==trimws("NA")),NA,ddata$pit)
 suppressWarnings(ddata$pit <- ifelse(!is.na(as.numeric("NA")) & (trimws(as.character(ddata$pit))==as.character(as.numeric("NA"))),NA,ddata$pit))
 
 
-#Exclude: 
-#-opened for two consecutive weeks every month for the period 16 June 1989 to 23 August 1991. Beginning after August 1991, traps were opened for two consecutive weeks quarterly: in February-March (following winter NPP measurements), in May-June (following spring NPP), in August, and in October-November (following fall NPP). 
+#Exclude:
+#-opened for two consecutive weeks every month for the period 16 June 1989 to 23 August 1991. Beginning after August 1991, traps were opened for two consecutive weeks quarterly: in February-March (following winter NPP measurements), in May-June (following spring NPP), in August, and in October-November (following fall NPP).
 #-> reduce data to October, November, August, Februar, March, May, June
 #- M-NORT grid installed 03/03/1995. Moved from M-RABB-C site. -exclude what?
 #-G-SUMM grid installed 06/06/1995. Moved from G-IBPE-A site
 # exclude double plots
 
-ddata <-ddata[!is.na(ddata$plot), ] #remove NA, I6-unknown location in site M-RABB
-ddata <- ddata[!(site == 'SAND' & plot == 'C')&!(site == 'RABB' & plot == 'C')&!(site == 'IBPE' & plot == 'C')] #remove plots uncomplete in time - reduce to 1 plot
 
-#rename columns & cleaning: 
-ddata <- unique(ddata[,.(date, zone, site, spp)])
-#dataset
-data.table::setnames(ddata, c("date","regional", "local", "species"))
+# Standardisation ----
+data.table::setnames(ddata, c("zone","spp"),c("regional","species"))
+ddata[, local := paste0(site, "_", plot)][, year := as.integer(format(date, "%Y"))][, species := as.character(species)]
+## in 2005 and 2006, all empty pits were coded as pit 0, even if several pits were empty? this would lead to a significant underestimation of effort and we decide to exclude 2005 and 2006 entirely. ----
+ddata <- ddata[!year %in% 2005L:2006L]
+## exclude subsampled sites ----
+ddata <- ddata[!is.na(plot)] #remove NA, I6-unknown location in site M-RABB
+ddata <- ddata[!(site == 'SAND' & plot == 'C') & !(site == 'RABB' & plot == 'C') & !(site == 'IBPE' & plot == 'C')] # remove plots uncomplete in time - reduce to 1 plot
+## resample individuals, based on the total abundance in the smallest sampling effort ----
+### computing effort ----
+ddata[, effort := length(unique(date)) * length(unique(pit)), by = .(year, regional, local)] # Effort is the minimal number of sampling operations ie the number of pitfall traps * the number of dates per local per year.
 
+### pooling pits and dates ----
+ddata <- ddata[, .(value = .N, effort = unique(effort)), by = .(year, regional, local, species)]
+### computing min total abundance for the local/year where the effort is the smallest ----
+ddata[, sample_size := sum(value), by = .(local, year)]
+min_sample_size <- ddata[effort == min(effort), min(sample_size)]
+### resampling ----
+source("./R/functions/resampling.r")
+set.seed(42)
+ddata[sample_size > min_sample_size, value := resampling(species, value, min_sample_size), by = .(local, year)]
+ddata[sample_size < min_sample_size, value := resampling(species, value, min_sample_size, replace = TRUE), by = .(local, year)]
+ddata <- ddata[!is.na(value)]
+
+# communities ----
 ddata[, ":="(
-  dataset_id = dataset_id,
-  value = 1,
-  metric = "pa",
-  unit = "pa"
+   dataset_id = dataset_id,
+
+   metric = "abundance",
+   unit = "count",
+
+   sample_size = NULL,
+   effort = NULL
 )]
-#metadata
-meta <- unique(ddata[, .(date, regional, local, plot)])
 
+
+# metadata ----
+meta <- unique(ddata[, .(dataset_id, year, regional, local)])
 meta[, ":="(
-  realm = "Terrestrial",
-  taxon = "Herpetofauna",
-  
-  latitude = '32°40`08.4000"N',
-  longitude = '106°51`54.0000"W',
-  
-  effort = 16L,  #plots per area?
-  
-  study_type = "ecological sampling", #two possible values, or NA if not sure 
-  
-  data_pooled_by_authors = FALSE, 
-  data_pooled_by_authors_comment = NA, 
-  sampling_years = NA, 
-  
-  alpha_grain = 90L,  #size/area of individual trap per local
-  alpha_grain_unit = "cm2", #"acres", "ha", "km2", "m2", "cm2"
-  alpha_grain_type = "trap", 
-  alpha_grain_comment = "15 cm2 diameter pitfall traps",
-  
-  gamma_bounding_box = 120L, #size of biggest common scale can be different values for different areas per region
-  gamma_bounding_box_unit = "km2", 
-  gamma_bounding_box_type = "box", 
-  gamma_bounding_box_comment = "complete area in which the 11 plots are located",
-  
-  gamma_sum_grains = 90L, #90x number of sites per year  
-  gamma_sum_grains_unit = "m2",
-  gamma_sum_grains_type = "plot",
-  gamma_sum_grains_comment = "Each grid consisted of 4 x 4 rows of traps spaced at 15 meter intervals",
+   realm = "Terrestrial",
+   taxon = "Herpetofauna",
 
-  comment = ""
-  )]
+   latitude = '32°40`08.4000"N',
+   longitude = '106°51`54.0000"W',
 
+   study_type = "ecological sampling", #two possible values, or NA if not sure
 
-#check standartisation in time - questionable effort equality
-ym <- data.frame(substr(ddata$date, 1, 7))
-ym <- as.data.frame(table(ym))
-ym$Freq
+   data_pooled_by_authors = FALSE,
+   data_pooled_by_authors_comment = NA,
+   sampling_years = NA,
 
-m <- data.frame(substr(ddata$date, 6, 7))
-m <- as.data.frame(table(m))
-m$Freq
+   effort = 24L, # Effort is the minimal number of sampling operations ie the number of pitfall traps * the number of dates per local per year
 
+   alpha_grain = 90L,  #size/area of individual trap
+   alpha_grain_unit = "cm2", #"acres", "ha", "km2", "m2", "cm2"
+   alpha_grain_type = "trap",
+   alpha_grain_comment = "15 cm2 diameter pitfall traps",
 
-###understanding data:
-#check how many samples per date
-date_points =ddata %>% group_by(date) %>% count(date)
-ggplot(date_points, aes(x = date, y = n)) +  
-  geom_bar(stat = "identity") + theme(axis.text.x = element_text(size = 2,angle = 60, hjust = 1))
-###not normally distibuted 
+   gamma_bounding_box = 120L, #size of biggest common scale can be different values for different areas per region
+   gamma_bounding_box_unit = "km2",
+   gamma_bounding_box_type = "box",
+   gamma_bounding_box_comment = "complete area in which the 11 plots are located",
 
-#check for how many individuals sampled per plot & date 
-individuals_per_plot = ddata %>% group_by(date, plot, site) %>% count(date, plot)
-ggplot(individuals_per_plot, aes(x = date, y = n)) +  
-  geom_bar(stat = "identity") + facet_wrap( ~ site) +
-  theme(axis.text.x = element_text(size = 2,angle = 60, hjust = 1))
+   gamma_sum_grains = 90L * 24L, #90 x effort
+   gamma_sum_grains_unit = "cm2",
+   gamma_sum_grains_type = "plot",
+   gamma_sum_grains_comment = "Each grid consisted of 4 x 4 rows of traps spaced at 15 meter intervals",
 
-#check which plot for site, how many entries
-individ_plot_site = ddata %>% group_by( plot, local) %>% count(local)
-ggplot(individ_plot_site, aes(x = plot, y = n)) +  
-  geom_bar(stat = "identity") + facet_wrap( ~ local) +
-  theme(axis.text.x = element_text(size = 2,angle = 60, hjust = 1))
+   comment = "Data extracted from EDI repository https://portal.edirepository.org/nis/mapbrowse?scope=knb-lter-jrn&identifier=210007001&revision=38 . The authors captured, marked and recaptured lizards in 4 zones, 2 to 3 plots per zone and a 4*4 grid of pitfal traps. Data is provided at the individual level per pitfall trap and we applied standardisation(described in comment_standardisation). Effort is the minimal number of sampling operations ie the number of pitfall traps * the number of dates per local per year.",
+   comment_standardisation = "data from 2005 and 2006 are excluded because empty pits are underestimated. only sites resampled in the 2000s are included. because effort varies: varying number of traps and varying number of sampling events per year, individuals are resampled down to the minimal number of captured individuals among the least intensively sampled years i.e. 12 individuals."
+)]
+
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
+                   row.names = FALSE
+)
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
+                   row.names = FALSE
+)
