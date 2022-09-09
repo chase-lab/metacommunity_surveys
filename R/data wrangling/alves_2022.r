61c093d3d620278286e6a4b9d74ffe83cc3ab67c
Alban Sagouis <alban.sagouis@idiv.de>
2022-09-09 10:42:28 +0200

    splitting alves_2022

--- R/data wrangling/alves_2022.r
+++ R/data wrangling/alves_2022.r
@@ -1,52 +1,44 @@
 dataset_id <- "alves_2022"
 
 ddata <- base::readRDS(file = "./data/raw data/alves_2022/rdata.rds")
+
+# Raw data ----
 ddata <- ddata[ Cover != 0 ][, Cover := NULL]
-ddata <- ddata[ !General.Type %in% c("Substrate","Dead","Equipment","Fish","Rubble","Sand.sediment","Unknown","Water","N.c","CTB")][, ":="(Specific.Type = NULL, General.Type = NULL)]
-ddata <- unique(ddata)
+ddata <- unique(ddata[ !General.Type %in% c("Substrate","Dead","Equipment","Fish","Rubble","Sand.sediment","Unknown","Water","N.c","CTB")][, ":="(Specific.Type = NULL, General.Type = NULL)])
 
 data.table::setnames(ddata, c("year","local","transect","species"))
 
-# Standardisation of effort ----
-# For each site, exclude transects sampled only once, then randomly select one transect. data.table style join
-set.seed(42)
-ddata <- ddata[
-   ddata[,
-         .(n_surveys = length(unique(year))),
-         by = .(local, transect)][n_surveys != 1L
-         ][,
-           .(transect = transect[sample(x = 1:.N, size = 1L)]),
-           by = local],
-
-   on = c("local", "transect")
-]
-
-# GIS Data ----
-coords <- sf::st_read("./data/GIS data/alves_2022_site_coordinates.kml")
-data.table::setDT(coords)
-data.table::setnames(coords, "Name","local")
-coords[, c("longitude", "latitude","z") := do.call(rbind.data.frame, geometry)]
-coords[, c("geometry", "Description", "z") := NULL]
-
-
-# community data ----
+## communities ----
 ddata[, ":="(
    dataset_id = dataset_id,
    regional = "Belizean Barrier Reef",
 
+   local = paste(sep = "-", local, transect),
+
    value = 1L,
    metric = "pa",
-   unit = "pa"
+   unit = "pa",
+
+   transect = NULL
 )]
 
-# metadata ----
-meta <- unique(ddata[, .(dataset_id, regional, local, year)])
-meta <- coords[meta, on = "local"]
+
+## GIS Data ----
+coords <- sf::st_read("./data/GIS data/alves_2022_site_coordinates.kml")
+data.table::setDT(coords)
+data.table::setnames(coords, "Name","site")
+coords[, c("longitude", "latitude","z") := do.call(rbind.data.frame, geometry)]
+coords[, c("geometry", "Description", "z") := NULL]
+
+
+## metadata ----
+meta <- unique(ddata[, .(dataset_id, regional, local, year, site = gsub(pattern = "-.*$", replacement = "", x = local))])
+meta <- coords[meta, on = "site"]
 meta[, ":="(
    taxon = "Invertebrates",
    realm = "Marine",
 
-   effort = 6L,
+   effort = 1L,
 
    study_type = "ecological_sampling",
 
@@ -67,20 +59,69 @@ meta[, ":="(
    gamma_bounding_box_comment = "sites located by hand on maps",
 
    comment = "Data were downloaded from https://github.com/calves06/Belizean_Barrier_Reef_Change associated to the article: Alves C, Valdivia A, Aronson RB, Bood N, Castillo KD, et al. (2022) Twenty years of change in benthic communities across the Belizean Barrier Reef. PLOS ONE 17(1): e0249155. https://doi.org/10.1371/journal.pone.0249155. Authors measured cover of the substrate by recording images along transects. Site coordinates were looked for on maps",
-   comment_standardisation = "Items from the following types were excluded: Substrate, Dead, Equipment, Fish, Rubble, Sand.sediment, Unknown, Water, N.c, CTB. For each location, we excluded transects sampled only once, then randomly selected one transect per year."
+   comment_standardisation = "Items from the following types were excluded: Substrate, Dead, Equipment, Fish, Rubble, Sand.sediment, Unknown, Water, N.c, CTB.",
+   site = NULL
 )][, ":="(
    gamma_sum_grains = sum(alpha_grain),
-   gamma_bounding_box = geosphere::areaPolygon(coords[grDevices::chull(coords$longitude, coords$latitude), c("longitude", "latitude")]) / 10^6),
+   gamma_bounding_box = geosphere::areaPolygon(na.omit(data.frame(latitude, longitude))[grDevices::chull(na.omit(longitude), na.omit(latitude)), c("longitude", "latitude")]) / 10^6),
    by = .(regional, year)]
+## Saving raw data ----
 
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
 data.table::fwrite(
    x = ddata,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
    row.names = FALSE
 )
 data.table::fwrite(
    x = meta,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+   row.names = FALSE
+)
+
+
+
+# Standardised data ----
+## Standardisation of effort ----
+# For each site, exclude transects sampled only once, then randomly select one transect. data.table style join
+ddata[, c("local", "transect") := data.table::tstrsplit(local, "-", fixed = TRUE)]
+set.seed(42)
+ddata <- ddata[
+   ddata[,
+         .(n_surveys = length(unique(year))),
+         by = .(local, transect)][n_surveys != 1L
+         ][,
+           .(transect = transect[sample(x = 1:.N, size = 1L)]),
+           by = local],
+
+   on = c("local", "transect")
+]
+
+
+## community data ----
+ddata[, transect := NULL]
+
+## metadata ----
+meta[, c("local", "transect") := data.table::tstrsplit(local, "-", fixed = TRUE)]
+meta[, transect := NULL]
+meta <- unique(unique(meta)[ddata[, .(regional, local, year)], on = .(regional, local, year)])
+
+meta[, ":="(
+   comment = "Data were downloaded from https://github.com/calves06/Belizean_Barrier_Reef_Change associated to the article: Alves C, Valdivia A, Aronson RB, Bood N, Castillo KD, et al. (2022) Twenty years of change in benthic communities across the Belizean Barrier Reef. PLOS ONE 17(1): e0249155. https://doi.org/10.1371/journal.pone.0249155. Authors measured cover of the substrate by recording images along transects. Site coordinates were looked for on maps",
+   comment_standardisation = "Items from the following types were excluded: Substrate, Dead, Equipment, Fish, Rubble, Sand.sediment, Unknown, Water, N.c, CTB. For each location, we excluded transects sampled only once, then randomly selected one transect per year."
+)][, ":="(
+   gamma_sum_grains = sum(alpha_grain),
+   gamma_bounding_box = geosphere::areaPolygon(data.frame(latitude, longitude)[grDevices::chull(longitude, latitude), c("longitude", "latitude")]) / 10^6),
+   by = .(regional, year)]
+
+## Saving standardised data ----
+data.table::fwrite(
+   x = ddata,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised.csv"),
+   row.names = FALSE
+)
+data.table::fwrite(
+   x = meta,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised_metadata.csv"),
    row.names = FALSE
 )
