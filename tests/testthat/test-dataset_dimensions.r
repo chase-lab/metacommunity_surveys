eeaad9dec2ae5156434ca71a10b7f0e8400c6dd5
Alban Sagouis <alban.sagouis@idiv.de>
2022-10-04 14:59:46 +0200

    improving automatic testing of intermediate data

--- tests/testthat/test-dataset_dimensions.r
+++ tests/testthat/test-dataset_dimensions.r
@@ -1,36 +1,83 @@
 # Paths to datasets ----
 absolute_path <- rprojroot::find_rstudio_root_file()
-listfiles <- list.files(
+
+## raw data ----
+listfiles_raw <- list.files(
    path = paste0(absolute_path, "/data/wrangled data"),
    pattern = "_raw.csv",
    full.names = TRUE, recursive = TRUE
 )
-listfiles_metadata <- list.files(
+listfiles_raw_metadata <- list.files(
    path = "./data/wrangled data",
    pattern = "raw_metadata.csv",
    full.names = TRUE, recursive = TRUE
 )
 
+## standardised data ----
+listfiles_standardised <- list.files(
+   path = paste0(absolute_path, "/data/wrangled data"),
+   pattern = "_standardised.csv",
+   full.names = TRUE, recursive = TRUE
+)
+listfiles_standardised_metadata <- list.files(
+   path = "./data/wrangled data",
+   pattern = "standardised_metadata.csv",
+   full.names = TRUE, recursive = TRUE
+)
+
 # Testing column names ----
-lst_column_names <- sapply(
-   X = listfiles,
+
+## raw data ----
+lst_column_names_raw <- sapply(
+   X = listfiles_raw,
    FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", nrows = 1L, header = FALSE
 )
 
-template <- utils::read.table(file = paste0(absolute_path, "./data/template_communities.txt"), header = TRUE, sep = "\t")
-column_names_template <- template[, 1]
+template_raw <- utils::read.table(file = paste0(absolute_path, "./data/template_communities.txt"), header = TRUE, sep = "\t")
+column_names_reference_raw <- template_raw[, 1]
 
 test_that(desc = "only valid column names - raw data", code =
-             for (i in listfiles) expect_setequal(lst_column_names[[i]], column_names_template)
+             for (i in listfiles_raw) {
+                tested_column_names <-  lst_column_names_raw[[i]]
+                expect_true(all(tested_column_names %in% column_names_reference_raw), info = i)
+             }
+)
+
+## standardised data ----
+lst_column_names_standardised <- sapply(
+   X = listfiles_standardised,
+   FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", nrows = 1L, header = FALSE
+)
+
+template_standardised <- utils::read.table(file = paste0(absolute_path, "./data/template_communities.txt"), header = TRUE, sep = "\t")
+column_names_reference_standardised <- template_standardised[, 1]
+
+test_that(desc = "only valid column names - standardised data", code =
+             for (i in listfiles_standardised) {
+                tested_column_names <-  lst_column_names_standardised[[i]]
+                expect_true(all(tested_column_names %in% column_names_reference_standardised), info = i)
+             }
 )
 
 
-# Testing data dimension
-lst_one_column <- sapply(X = listfiles, FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", select = "value")
-lst_metadata_one_column <- sapply(X = listfiles_metadata, FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", select = "year")
 
 
-test_that(desc = "ddata has mor rows than meta", code =
-             expect_gte(sum(sapply(lst_one_column, nrow)), sum(sapply(lst_metadata_one_column, nrow)))
+# Testing data dimension ----
+## raw data ----
+lst_one_column_raw <- sapply(X = listfiles_raw, FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", select = "value")
+lst_metadata_one_column_raw <- sapply(X = listfiles_raw_metadata, FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", select = "year")
+
+
+test_that(desc = "ddata has mor rows than meta - raw data", code =
+             expect_gte(sum(sapply(lst_one_column_raw, nrow)), sum(sapply(lst_metadata_one_column_raw, length)))
+)
+
+## standardised data ----
+lst_one_column_standardised <- sapply(X = listfiles_standardised, FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", select = "value")
+lst_metadata_one_column_standardised <- sapply(X = listfiles_standardised_metadata, FUN = data.table::fread, integer64 = "character", encoding = "UTF-8", sep = ",", select = "year")
+
+
+test_that(desc = "ddata has mor rows than meta - standardised data", code =
+             expect_gte(sum(sapply(lst_one_column_standardised, length)), sum(sapply(lst_metadata_one_column_standardised, length)))
 )
 
