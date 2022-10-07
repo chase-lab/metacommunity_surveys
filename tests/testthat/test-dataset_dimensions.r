a69fe56a61e1e819c1b5aeec2efc3020934dd06e
cachaos <cawamaa@gmx.de>
2022-10-07 19:43:25 +0200

    updated tests with input from pre-commit hook script

--- tests/testthat/test-dataset_dimensions.r
+++ tests/testthat/test-dataset_dimensions.r
@@ -1,29 +1,47 @@
 # Paths to datasets ----
 absolute_path <- rprojroot::find_rstudio_root_file()
 
+#read input argument: edited files since last commit
+args <- commandArgs()
+listfiles <- tail(args,-5)
+
 ## raw data ----
 listfiles_community_raw <- list.files(
    path = paste0(absolute_path, "/data/wrangled data"),
    pattern = "_raw.csv",
    full.names = TRUE, recursive = TRUE
 )
+
+listfiles_community_raw <- unique (grep(paste(listfiles,collapse="|"), 
+                                        listfiles_community_raw, value=TRUE))
+
+
 listfiles_metadata_raw <- list.files(
    path = "/data/wrangled data",
    pattern = "raw_metadata.csv",
    full.names = TRUE, recursive = TRUE
 )
-
+listfiles_metadata_raw<- unique (grep(paste(listfiles,collapse="|"), 
+                                      listfiles_metadata_raw, value=TRUE))
 ## standardised data ----
 listfiles_community_standardised <- list.files(
    path = paste0(absolute_path, "/data/wrangled data"),
    pattern = "_standardised.csv",
    full.names = TRUE, recursive = TRUE
 )
+
+listfiles_community_standardised<- unique (grep(paste(listfiles,collapse="|"), 
+                                                listfiles_community_standardised, value=TRUE))
+
+
 listfiles_metadata_standardised <- list.files(
    path = "/data/wrangled data",
    pattern = "standardised_metadata.csv",
    full.names = TRUE, recursive = TRUE
 )
+
+listfiles_metadata_standardised<- unique (grep(paste(listfiles,collapse="|"), 
+                                      listfiles_metadata_standardised, value=TRUE))
 
 # Testing column names ----
 ## community data ----
