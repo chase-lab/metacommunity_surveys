80b5bdc35a463e51d17dec4ff5ea3056dfc2fe0a
cachaos <cawamaa@gmx.de>
2022-11-21 16:24:35 +0100

    delete reed_2022, split rogalski_2017, russel-smith_2017_trees, santana_2017, schuch_2011

--- R/data wrangling/russel-smith_2017_trees.r
+++ R/data wrangling/russel-smith_2017_trees.r
@@ -7,134 +7,153 @@ dataset_id <- "russell-smith_2017_trees"
 ###  https://datacommons.anu.edu.au/russell-smith_2017/dataCommons/rest/records/anudc:5837/data/
 ###  Login for Australian National University needed. Data accessible after login without further requests.
 
-# loading data ----
+#Raw Data ----
+##Loading data ----
 datafiles <- c(
-"./data/raw data/russell-smith_2017/data/tpsk_trees_1994+_p831t1066.csv",
-"./data/raw data/russell-smith_2017/data/tpsl_trees_1994+_p831t1124.csv",
-"./data/raw data/russell-smith_2017/data/tpsn_trees_1994+_p831t1129.csv"
+   "./data/raw data/russell-smith_2017/data/tpsk_trees_1994+_p831t1066.csv",
+   "./data/raw data/russell-smith_2017/data/tpsl_trees_1994+_p831t1124.csv",
+   "./data/raw data/russell-smith_2017/data/tpsn_trees_1994+_p831t1129.csv"
 )
 
 datafiles_dates <- c(
-"./data/raw data/russell-smith_2017/data/tpsk_visit_date_1994+_p831t1067.csv",
-"./data/raw data/russell-smith_2017/data/tpsl_visit_date_1994+_p831t1125.csv",
-"./data/raw data/russell-smith_2017/data/tpsn_visit_date_1994+_p831t1153.csv"
+   "./data/raw data/russell-smith_2017/data/tpsk_visit_date_1994+_p831t1067.csv",
+   "./data/raw data/russell-smith_2017/data/tpsl_visit_date_1994+_p831t1125.csv",
+   "./data/raw data/russell-smith_2017/data/tpsn_visit_date_1994+_p831t1153.csv"
 )
 
 datafiles_spatial <- c(
-"./data/raw data/russell-smith_2017/spatial/tpsk_plot_details_spatial_coordinates_p894t1154.csv",
-"./data/raw data/russell-smith_2017/spatial/tpsl_plot_details_spatial_coordinates_p894t1155.csv",
-"./data/raw data/russell-smith_2017/spatial/tpsn_plot_details_spatial_coordinates_p894t1156.csv"
+   "./data/raw data/russell-smith_2017/spatial/tpsk_plot_details_spatial_coordinates_p894t1154.csv",
+   "./data/raw data/russell-smith_2017/spatial/tpsl_plot_details_spatial_coordinates_p894t1155.csv",
+   "./data/raw data/russell-smith_2017/spatial/tpsn_plot_details_spatial_coordinates_p894t1156.csv"
 )
 
 
 
 ddata <- data.table::rbindlist(
-fill = TRUE,
-lapply(
-   datafiles,
-   FUN = function(x)
-      data.table::fread(file = x)
-),
-use.names = TRUE, idcol = FALSE
+   fill = TRUE,
+   lapply(
+      datafiles,
+      FUN = function(x)
+         data.table::fread(file = x)
+   ),
+   use.names = TRUE, idcol = FALSE
 )
 
 dates <- data.table::rbindlist(
-lapply(
-   datafiles_dates,
-   FUN = function(x)
-      data.table::fread(file = x)
-),
-use.names = TRUE, idcol = FALSE
+   lapply(
+      datafiles_dates,
+      FUN = function(x)
+         data.table::fread(file = x)
+   ),
+   use.names = TRUE, idcol = FALSE
 )
 
 spatial <- data.table::rbindlist(
-lapply(
-   datafiles_spatial,
-   FUN = function(x)
-      data.table::fread(file = x)
-),
-use.names = TRUE, idcol = FALSE, fill = TRUE
+   lapply(
+      datafiles_spatial,
+      FUN = function(x)
+         data.table::fread(file = x)
+   ),
+   use.names = TRUE, idcol = FALSE, fill = TRUE
 )
 
-#### Trees are defined as any woody species with diameter at breast height (DBH) > 5cm.
-# excluding unknown species
-ddata <- ddata[genus_species != ""]
-
-# merge data and dates
+#Raw Data ----
+##merge data and dates ----
 ddata <- dates[ddata, on = c("park", "plot", "visit")]
 ddata <- ddata[,.N, by = .(park, plot, visit, genus_species, date)]
 
 data.table::setnames(ddata, c("park", "plot","genus_species", "N"), c("regional","local","species", "value"))
 
-#format spatial data to have common identifier with ddata
+##format spatial data to have common identifier with species data ----
 spatial[, regional := c("Kakadu","Litchfield","Nitmiluk")[match(substr(plot, 1, 3), c("KAK","LIT","NIT"))]]
 spatial[, local := stringi::stri_extract_all_regex(str = plot, pattern = "[0-9]{2,3}")
 ][, local := as.integer(sub("^0+(?=[1-9])", "", local, perl = TRUE))]
 
+##community data ----
 
 ddata[, ":="(
-dataset_id = dataset_id,
-year = format(date, "%Y"),
-
-visit = NULL,
-date = NULL,
-
-metric = "abundance",
-unit = "count"
+   dataset_id = dataset_id,
+   year = format(date, "%Y"),
+   
+   visit = NULL,
+   date = NULL,
+   
+   metric = "abundance",
+   unit = "count"
 )]
-
-# remove NA values in year because of missing dates in original data
-ddata <- na.omit(ddata, cols = "year")
-
-# deleting site sampled only once with a data.table style join
-ddata <- ddata[ ddata[, .(n_years = length(unique(year))), by = .(regional, local)][n_years > 1L][, .(regional, local)], on = .(regional, local)]
 
 meta <- unique(ddata[, .(dataset_id, year, regional, local)])
 meta <- spatial[, .(local, regional, latitude, longitude)][meta, on = c("local", "regional")]
 
 meta[, ":="(
-realm = "Terrestrial",
-taxon = "Plants",
+   realm = "Terrestrial",
+   taxon = "Plants",
+   
+   study_type = "ecological_sampling", #two possible values, or NA if not sure
+   
+   data_pooled_by_authors = FALSE,
+   data_pooled_by_authors_comment = NA,
+   sampling_years = NA,
+   
+   alpha_grain = 800L,  #area of individual plot
+   alpha_grain_unit = "m2", #"acres", "ha", "km2", "m2", "cm2"
+   alpha_grain_type = "plot",
+   alpha_grain_comment = "all trees defined as wooden species with diameter at breast hight > 5cm are counted in 40*20m plot ",
+   
+   comment = "Data manually downloaded via https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5836/data/ with login for national university of australia webpage. Authors sampled trees with DBH (diameter at breast hight) > 5cm in fixed 40m*20m plots once a year.",
+   comment_standardisation = "None")]
 
-study_type = "ecological_sampling", #two possible values, or NA if not sure
+##save data ----
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+                   row.names = FALSE
+)
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+                   row.names = FALSE
+)
 
-data_pooled_by_authors = FALSE,
-data_pooled_by_authors_comment = NA,
-sampling_years = NA,
+#Standardized Data -----
 
-effort = 1L, # Effort is the minimal number of sampling operations
+##excluding unknown species ----
+ddata <- ddata[species != ""]
 
-alpha_grain = 800L,  #area of individual plot
-alpha_grain_unit = "m2", #"acres", "ha", "km2", "m2", "cm2"
-alpha_grain_type = "plot",
-alpha_grain_comment = "all trees defined as wooden species with diameter at breast hight > 5cm are counted in 40*20m plot ",
+##remove NA values in year because of missing dates in original data ----
+ddata <- na.omit(ddata, cols = "year")
+
+##deleting site sampled only once with a data.table style join ----
+ddata <- ddata[ ddata[, .(n_years = length(unique(year))), by = .(regional, local)][n_years > 1L][, .(regional, local)], on = .(regional, local)]
 
 
-gamma_sum_grains_unit = "m2",
-gamma_sum_grains_type = "plot",
-gamma_sum_grains_comment = "area of the sampled plots per year multiplied by amount of plots per region",
+##meta data ----
+meta <- meta[unique(ddata[, .(dataset_id, local, regional, year)]),
+             on = .(local, regional, year)]
 
-gamma_bounding_box_unit = "km2",
-gamma_bounding_box_type = "convex-hull",
-gamma_bounding_box_comment = "convex-hull over the coordinates of sample points",
-
-comment = "Data manually downloaded via https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5836/data/ with login for national university of australia webpage. Authors sampled trees with DBH (diameter at breast hight) > 5cm in fixed 40m*20m plots once a year.",
-comment_standardisation = "some visit numbers (T1, T2,...) have no match (year) in the dates table so they were excluded. Sites sampled only one year were excluded."
+meta[,":="(
+   effort = 1L,
+   
+   gamma_sum_grains_unit = "m2",
+   gamma_sum_grains_type = "plot",
+   gamma_sum_grains_comment = "area of the sampled plots per year multiplied by amount of plots per region",
+   
+   gamma_bounding_box_unit = "km2",
+   gamma_bounding_box_type = "convex-hull",
+   gamma_bounding_box_comment = "convex-hull over the coordinates of sample points",
+   
+   comment_standardisation = "some visit numbers (T1, T2,...) have no match (year) in the dates table so they were excluded. Sites sampled only one year were excluded."
 )]
 
-
 meta[, ":="(
-gamma_bounding_box = geosphere::areaPolygon(data.frame(na.omit(longitude), na.omit(latitude))[grDevices::chull(na.omit(longitude), na.omit(latitude)), ]) / 10^6,
-gamma_sum_grains = sum(alpha_grain)
+   gamma_bounding_box = geosphere::areaPolygon(data.frame(na.omit(longitude), na.omit(latitude))[grDevices::chull(na.omit(longitude), na.omit(latitude)), ]) / 10^6,
+   gamma_sum_grains = sum(alpha_grain)
 ),
 by = .(regional, year)
 ]
 
-
+##save data ----
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
-data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
-                row.names = FALSE
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized.csv"),
+                   row.names = FALSE
 )
-data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
-                row.names = FALSE
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized_metadata.csv"),
+                   row.names = FALSE
 )
--- R/data wrangling/santana_2017.r
+++ R/data wrangling/santana_2017.r
@@ -1,8 +1,8 @@
-## santana_2017
-
 
 dataset_id <- "santana_2017"
 ddata <- base::readRDS(file = paste0("data/raw data/", dataset_id, "/ddata.rds"))
+
+#Raw Data ----
 
 data.table::setnames(
   ddata, c("Year", "Transect", "Date"),
@@ -10,64 +10,85 @@ data.table::setnames(
 )
 
 ddata <- data.table::melt(ddata,
-  id.vars = c("year", "local", "Rowname", "Farmland_area", "date", "Recorder"),
-  variable.name = "species",
-  value.name = "value"
+                          id.vars = c("year", "local", "Rowname", "Farmland_area", "date", "Recorder"),
+                          variable.name = "species",
+                          value.name = "value"
 )
+
 ddata <- ddata[value > 0]
 
-# One sampling per year
-# table(ddata[,.(tot = length(unique(date))), by = .(local, year)]$tot)
-
-
+##community data ----
 ddata[, ":="(
   dataset_id = dataset_id,
   regional = paste("SPA Castro Verde", Farmland_area),
-
+  
   metric = "abundance",
   unit = "count",
-
+  
   date = NULL,
   Rowname = NULL,
   Recorder = NULL,
   Farmland_area = NULL
 )]
 
+##meta data ----
+
 meta <- unique(ddata[, .(dataset_id, regional, local, year)])
 meta[, ":="(
   realm = "Terrestrial",
   taxon = "Birds",
-
+  
   latitude = data.table::fifelse(grepl("High-intensity", regional), "38°03'N", "37°41N"),
   longitude = data.table::fifelse(grepl("High-intensity", regional), "8°06'W", "8°00'W"),
-
-  effort = 1L,
+  
   study_type = "ecological_sampling",
-
+  
   data_pooled_by_authors = FALSE,
-
+  
   alpha_grain = 32.12,
   alpha_grain_unit = "ha",
   alpha_grain_type = "sample",
   alpha_grain_comment = "transect 250m buffer",
+  
+  comment = "Extracted from Santana et al 2017 Dryad repo (https://datadryad.org/stash/dataset/doi:10.5061/dryad.kp3fv). Authors sampled birds in two rural areas of Portugal. Both areas differ by the agriculture intensity. No standardization needed, one sampling per year during 6 years, values are numbers of individual per transect. Each local is a buffer of 250 m along a transect (71 transects).",
+  comment_standardisation = "none needed"
+)]
 
+##save data ----
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+                   row.names = FALSE
+)
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+                   row.names = FALSE
+)
+
+#Standardized Data ----
+
+##meta data ----
+meta <- meta[unique(ddata[, .(dataset_id, local, regional, year)]),
+             on = .(local, regional, year)]
+meta[,":="(
+  effort = 1L,
+  
   gamma_sum_grains_unit = "ha",
   gamma_sum_grains_type = "sample",
   gamma_sum_grains_comment = "sum of the sampled transect areas per region per year",
-
+  
   gamma_bounding_box = data.table::fifelse(grepl("High-intensity", regional), 423.92, 456.08),
   gamma_bounding_box_unit = "km2",
   gamma_bounding_box_type = "functional",
   gamma_bounding_box_comment = "areas of the high and low intensity areas estimated from figure 1",
-
-  comment = "Extracted from Santana et al 2017 Dryad repo (https://datadryad.org/stash/dataset/doi:10.5061/dryad.kp3fv). Authors sampled birds in two rural areas of Portugal. Both areas differ by the agriculture intensity. No standardization needed, one sampling per year during 6 years, values are numbers of individual per transect. Each local is a buffer of 250 m along a transect (71 transects).",
+  
   comment_standardisation = "none needed"
 )][, gamma_sum_grains := 32.12 * length(unique(local)), by = .(regional, year)]
 
+##save data ----
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
-data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
-  row.names = FALSE
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized.csv"),
+                   row.names = FALSE
 )
-data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
-  row.names = FALSE
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized_metadata.csv"),
+                   row.names = FALSE
 )
+
