39e48a3a09e5d7a9a0831f3b0b8efd98ab8570df
cachaos <cawamaa@gmx.de>
2022-12-11 14:29:04 +0100

    splitting muschet_2017, moore_2022, medieta-leiva_2021

--- R/data wrangling/mendieta-leiva_2021.r
+++ R/data wrangling/mendieta-leiva_2021.r
@@ -7,60 +7,88 @@ ddata[, ":="(
    tsppc = NULL
 )]
 
-data.table::setnames(ddata, c("year", "local", "species", "value","alpha_grain"))
+##community data ----
 
-# selecting only tree communities that were sampled twice using a data.table joint
-ddata <- ddata[ ddata[, .(selec = length(unique(year)) == 2L), by = local][(selec), .(local)], on = "local"]
+data.table::setnames(ddata, c("year", "local", "species", "value","alpha_grain"))
 
 
 ddata[, ":="(
    dataset_id = dataset_id,
    regional = "San Lorenzo crane plot",
-
+   
    metric = "abundance",
    unit = "count"
 )]
 
+##meta data ----
 meta <- unique(ddata[, .(dataset_id, regional, local, year, alpha_grain)])
 meta[, ":="(
    taxon = "Plants",
    realm = "Terrestrial",
-
+   
    latitude = 9.2815,
    longitude = -79.974,
-
-   effort = 1L,
-
+   
    study_type = "resurvey",
-
+   
    data_pooled_by_authors = FALSE,
    sampling_years = c("1998-2000", "2010-2012")[match(year, c(2000L, 2012L))],
-
+   
    alpha_grain_unit = "m2",
    alpha_grain_type = "functional",
    alpha_grain_comment = "1 tree on which epiphytes were observed average crown area from the literature",
+   
+   comment = "Extracted from Zenodo repository from mendieta-leiva_2021. They sampled epiphytes on 200+ trees of the San Lorenzo crane plot in 1998-2000 and again in 2010-2012. We kept ony the trees that were actually sampled twice. Trees are considered local scale and the regional scale is the San Lorenzo crane plot which is a 140*140m plot inside the San Lorenzo National Park, Panama. Average crown area/ alpha grain was computed from Martinez Cano, Isabel et al. (2019), Data from: Tropical tree height and crown allometries for the Barro Colorado Nature Monument, Panama: a comparison of alternative hierarchical models incorporating interspecific variation in relation to life history traits, Dryad, Dataset, https://doi.org/10.5061/dryad.85k53v8",
+   comment_standardisation = "none"
+)]
 
+ddata[, alpha_grain := NULL]
+
+##save data ----
+
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+                   row.names = FALSE
+)
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+                   row.names = FALSE
+)
+
+#Standardized data ----
+## selecting only tree communities that were sampled twice using a data.table joint
+ddata <- ddata[ ddata[, .(selec = length(unique(year)) == 2L), by = local][(selec), .(local)], on = "local"]
+
+##meta data ----
+
+meta <- meta[unique(ddata[, .(dataset_id, local, regional, year)]),
+             on = .(local, regional, year)]
+
+meta[,":="(
+   effort = 1L,
+   
    gamma_bounding_box = 140L*140L,
    gamma_bounding_box_unit = "m2",
    gamma_bounding_box_type = "administrative",
    gamma_bounding_box_comment = "area of the San Lorenzo crane plot",
-
+   
    gamma_sum_grains_unit = "m2",
    gamma_sum_grains_type = "functional",
    gamma_sum_grains_comment = "sum of the sampled areas of 13 pools",
-
-   comment = "Extracted from Zenodo repository from mendieta-leiva_2021. They sampled epiphytes on 200+ trees of the San Lorenzo crane plot in 1998-2000 and again in 2010-2012. We kept ony the trees that were actually sampled twice. Trees are considered local scale and the regional scale is the San Lorenzo crane plot which is a 140*140m plot inside the San Lorenzo National Park, Panama. Average crown area/ alpha grain was computed from Martinez Cano, Isabel et al. (2019), Data from: Tropical tree height and crown allometries for the Barro Colorado Nature Monument, Panama: a comparison of alternative hierarchical models incorporating interspecific variation in relation to life history traits, Dryad, Dataset, https://doi.org/10.5061/dryad.85k53v8",
-   comment_standardisation = "none needed"
-)][,
-   gamma_sum_grains := sum(alpha_grain), by = year
+   
+   comment_standardisation ="selected only tree communities that were sampled twice"
+)
+][,
+  gamma_sum_grains := sum(alpha_grain), by = year
 ]
-ddata[, alpha_grain := NULL]
 
+
+##save data ----
 
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
-data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized.csv"),
                    row.names = FALSE
 )
-data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized_metadata.csv"),
                    row.names = FALSE
 )
+
--- R/data wrangling/muschet_2017.R
+++ R/data wrangling/muschet_2017.R
@@ -1,14 +1,15 @@
 dataset_id <- "mushet_2017"
 
+#Raw Data ----
 
-# Extracting taxonomy from metadata ----
+##Extracting taxonomy from metadata ----
 if (!file.exists("./data/raw data/muschet_2017/CLSAInvertebrateCounts_metadata_taxonomy.csv")) {
   tax <- XML::xmlToList("./data/raw data/muschet_2017/CLSAInvertebrateCounts_metadata.xml")
-
+  
   codes <- unlist(sapply(tax$eainfo[[1]], function(element) element$attrlabl))
   lnames <- unlist(sapply(tax$eainfo[[1]], function(element) element$attrdef))
   lnames <- stringi::stri_extract_first_regex(str = lnames, pattern = "(?<=identified as? ).*(?= present)")
-
+  
   write.table(
     x = na.omit(data.frame(codes = codes, long_names = lnames)),
     sep = ",", row.names = FALSE,
@@ -17,92 +18,119 @@ if (!file.exists("./data/raw data/muschet_2017/CLSAInvertebrateCounts_metadata_t
 }
 tax <- read.table("./data/raw data/muschet_2017/CLSAInvertebrateCounts_metadata_taxonomy.csv", sep = ",", header = TRUE)
 
-# Loading coordinates
+
+##Coordinates
 coords <- read.csv("./data/raw data/muschet_2017/site locations.csv", skip = 1)
 coords$Plot_name <- ifelse(nchar(coords$Plot_name) == 2, paste0(substr(coords$Plot_name, 1, 1), "0", substr(coords$Plot_name, 2, 2)), coords$Plot_name)
 
-# Reading insect counts ----
+##Reading insect counts ----
 ddata <- base::readRDS("./data/raw data/muschet_2017/rdata.rds")
 
-# melting species ----
+##melting species ----
 ddata <- data.table::melt(ddata,
-  id.vars = 1:5,
-  variable.name = "species",
-  na.rm = TRUE
+                          id.vars = 1:5,
+                          variable.name = "species",
+                          na.rm = TRUE
 )
 
 # when value is D, the pond was not sampled because it was dry
 ddata <- ddata[!value %in% c("0", "D", "")]
 
-# standardising ----
-# selecting only months when all 3 transects were sampled.
-ddata[, full_sample_month := length(unique(TRANSECT)), by = .(WETLAND, YEAR, MONTH)]
-ddata <- ddata[full_sample_month == 3L]
-# selecting only wetlands/years where all 6 months were sampled
-ddata[, nmonth := length(unique(MONTH)), by = .(WETLAND, YEAR)]
-ddata <- ddata[nmonth == 6L]
-#
-# # selecting the first two months for each WETLAND//YEAR
-# seldata <- unique(ddata[, .(WETLAND, YEAR, MONTH)])
-# seldata[, month_priority_order := c(2L, 1L, 3L, 4L)[match(MONTH, c(5, 6, 7, 8))]]
-# data.table::setorder(seldata, WETLAND, YEAR, month_priority_order)
-# seldata <- seldata[, .SD[1:2,], by = .(WETLAND, YEAR)]
-# # subsetting by using a data.table join
-# ddata <- ddata[seldata, on = c('WETLAND','YEAR','MONTH')]
-#
 
-# ddata[, .N / sum(grepl("\\.", value)), by = .(YEAR, WETLAND, TRANSECT, VEGZONE)]
-
-
-# pooling all MONTHs, TRANSECTs and VEGZONEs together
-ddata <- unique(ddata[, .(value = sum(as.numeric(value))), by = .(WETLAND, YEAR, species)])
-
-
-# ddata wrangling ----
-data.table::setnames(ddata, 1:2, c("local", "year"))
+##community data ----
+data.table::setnames(ddata, c("WETLAND","YEAR","MONTH"), c("local", "year", "month"))
 ddata[, ":="(
   dataset_id = dataset_id,
+  local = paste(local, sep="_", VEGZONE),
   regional = "Cottonwood Lake Study Area",
-
+  
   species = tax$long_names[match(species, tax$codes)],
-
+  
   metric = "abundance",
-  unit = "count"
+  unit = "count",
+  VEGZONE = NULL
 )]
 
-# metadta ----
+##meta data ----
 meta <- unique(ddata[, .(dataset_id, regional, local, year)])
 meta[, ":="(
   taxon = "Invertebrates",
   realm = "Freshwater",
-
+  
   latitude = coords$Latitude[match(local, coords$Plot_name)],
   longitude = coords$Longitude[match(local, coords$Plot_name)],
-
-  effort = 6L,
+  
   study_type = "ecological_sampling",
-
+  
   data_pooled_by_authors = FALSE,
-
+  
   alpha_grain = NA,
   alpha_grain_unit = NA,
   alpha_grain_type = "functional",
   alpha_grain_comment = "wetland area unknown",
+  
+  comment = "Extracted from Mushet, D.M., Euliss, N.H., Jr., and Solensky, M.J. 2017, Cottonwood Lake Study Area - Invertebrate Counts, U.S. Geological Survey data release, https://doi.org/10.5066/F7BK1B77. Authors provide data sampled in the Cottonwood Lake Study Area from 1992 to 2015. Methods: 'Aquatic macroinvertebrates were collected each month (April-September) from all wetlands at Cottonwood Lake Study Area containing water using vertically oriented funnel traps (Swanson 1978). Sampling was stratified to provide separate estimates of invertebrate biomass and abundance in all major vegetative zones of each wetland. Samples were collected at random locations along the 3 established transects in each wetland that were established earlier and used to collect other biotic and abiotic data (LaBaugh et al. 1987). The length of each vegetation zone as bisected by transects was measured and a computer-generated set of random numbers used to identify sample points for the collection of invertebrate samples in each vegetative zone. One sample was collected from each major vegetative zone from each transect. Data consist of counts by taxa.'  Taxonomic names were extracted from metadata file https://www.sciencebase.gov/catalog/item/get/599d9555e4b012c075b964a6",
+  comment_standardisation = "values of D, 0 and NA  were excluded because no sampling event happend"
+)]
 
+##save data ----
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(ddata[,!c("TRANSECT")], paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+                   row.names = FALSE
+)
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+                   row.names = FALSE
+)
+
+#Standaridzed Data----
+
+##selecting only months when all 3 transects were sampled ----
+ddata[, full_sample_month := length(unique(TRANSECT)), by = .(local, year, month)]
+ddata <- ddata[full_sample_month == 3L]
+##selecting only wetlands/years where all 6 months were sampled ----
+ddata[, nmonth := length(unique(month)), by = .(local, year)]
+ddata <- ddata[nmonth == 6L]
+
+# # selecting the first two months for each local//year
+# seldata <- unique(ddata[, .(local, year, month)])
+# seldata[, month_priority_order := c(2L, 1L, 3L, 4L)[match(month, c(5, 6, 7, 8))]]
+# data.table::setorder(seldata, local, year, month_priority_order)
+# seldata <- seldata[, .SD[1:2,], by = .(local, year)]
+# # subsetting by using a data.table join
+# ddata <- ddata[seldata, on = c('local','year','month')]
+#
+# ddata[, .N / sum(grepl("\\.", value)), by = .(year, local, TRANSECT, VEGZONE)]
+
+## pooling all months, TRANSECTs and VEGZONEs together
+ddata <- unique(ddata[, .(value = sum(as.numeric(value))), by = .(local, year, species)])
+
+##meta data ----
+
+meta <- meta[unique(ddata[, .(dataset_id, local, regional, year)]),
+             on = .(local, regional, year)]
+
+meta[,":="(
+  effort = 6L,
+  
+  gamma_sum_grains = NA,
+  gamma_sum_grains_type = "sample",
+  gamma_sum_grains_unit = "m2",
+  gamma_sum_grains_comment = "unknown number of trap per transect.",
+  
   gamma_bounding_box = geosphere::areaPolygon(coords[grDevices::chull(coords$Longitude, coords$Latitude), c("Longitude", "Latitude")]) / 1000000,
   gamma_bounding_box_unit = "km2",
   gamma_bounding_box_type = "convex-hull",
   gamma_bounding_box_comment = "area of the region computed as the convexhull covering the centres of all ponds",
-
-  comment = "Extracted from Mushet, D.M., Euliss, N.H., Jr., and Solensky, M.J. 2017, Cottonwood Lake Study Area - Invertebrate Counts, U.S. Geological Survey data release, https://doi.org/10.5066/F7BK1B77. Authors provide data sampled in the Cottonwood Lake Study Area from 1992 to 2015. Methods: 'Aquatic macroinvertebrates were collected each month (April-September) from all wetlands at Cottonwood Lake Study Area containing water using vertically oriented funnel traps (Swanson 1978). Sampling was stratified to provide separate estimates of invertebrate biomass and abundance in all major vegetative zones of each wetland. Samples were collected at random locations along the 3 established transects in each wetland that were established earlier and used to collect other biotic and abiotic data (LaBaugh et al. 1987). The length of each vegetation zone as bisected by transects was measured and a computer-generated set of random numbers used to identify sample points for the collection of invertebrate samples in each vegetative zone. One sample was collected from each major vegetative zone from each transect. Data consist of counts by taxa.'  Taxonomic names were extracted from metadata file https://www.sciencebase.gov/catalog/item/get/599d9555e4b012c075b964a6",
+  
   comment_standardisation = "To ensure standard effort, we kept only wetlands and years that were sampled in all 3 transects in each of the 6 months. Then, samples from all transects and vegetative zones of a site, of all months of a year were pooled together."
+  
 )]
 
+##save data ----
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
-data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
-  row.names = FALSE
+data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized.csv"),
+                   row.names = FALSE
 )
-data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
-  row.names = FALSE
+data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized_metadata.csv"),
+                   row.names = FALSE
 )
-
