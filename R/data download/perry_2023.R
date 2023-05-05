90a434bad3e9f6966c0b3557c10d95b9bc193174
AlbanSagouis <alban.sagouis@idiv.de>
2023-05-05 08:25:37 +0200

    update of perry_2023 from version 3 to version 4

--- R/data download/perry_2023.R
+++ R/data download/perry_2023.R
@@ -7,7 +7,7 @@
 # Contact:  Tiffany Brown -  CA Department of Water Resources  - tiffany.brown@water.ca.gov
 # Stylesheet v2.11 for metadata conversion into program: John H. Porter, Univ. Virginia, jporter@virginia.edu
 
-inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/1320/3/1eee2c2a562a5b856398082c487dc1a7"
+inUrl1  <- "https://pasta.lternet.edu/package/data/eml/edi/1320/4/1eee2c2a562a5b856398082c487dc1a7"
 infile1 <- 'data/cache/perry_2023.csv'
 if (is.na(file.size(infile1))) download.file(inUrl1,infile1,method="curl")
 if (is.na(file.size(infile1))) download.file(inUrl1,infile1,method="auto")
@@ -29,7 +29,7 @@ dt1$Organisms_per_mL <- ifelse((trimws(as.character(dt1$Organisms_per_mL))==trim
 suppressWarnings(dt1$Organisms_per_mL <- ifelse(!is.na(as.numeric("NA")) & (trimws(as.character(dt1$Organisms_per_mL))==as.character(as.numeric("NA"))),NA,dt1$Organisms_per_mL))
 
 
-inUrl2  <- "https://pasta.lternet.edu/package/data/eml/edi/1320/3/00b61678378071f779f1135be223fe0a"
+inUrl2  <- "https://pasta.lternet.edu/package/data/eml/edi/1320/4/00b61678378071f779f1135be223fe0a"
 infile2 <- 'data/cache/perry_2023_environment.csv'
 if (is.na(file.size(infile2))) download.file(inUrl2 ,infile2, method = "curl")
 if (is.na(file.size(infile2))) download.file(inUrl2,infile2, method = "auto")
