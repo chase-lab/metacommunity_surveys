6ac8c275300eda7deaeaaee62edc291414e063ac
AlbanSagouis <alban.sagouis@idiv.de>
2023-08-02 12:25:04 +0200

    splitting klinkovska_2022

--- R/data wrangling/klinkovska_2022.R
+++ R/data wrangling/klinkovska_2022.R
@@ -4,8 +4,8 @@ ddata <- base::readRDS(file = 'data/raw data/klinkovska_2022/rdata.rds')
 data.table::setnames(x = ddata,
                      old = c('Rs_plot', 'deg_lat', 'deg_lon', 'Date..year.month.day.', 'Releve.area..m2.'),
                      new = c('local','latitude','longitude','date','alpha_grain'))
-
-# melting species ----
+# Raw data ----
+## melting species ----
 species_list <- base::grep('_[0-9]$', colnames(ddata), value = TRUE, perl = TRUE)
 ddata[, (species_list) := lapply(.SD,
                                  function(column) replace(column, column == 0, NA_real_)),
@@ -18,24 +18,24 @@ ddata <- data.table::melt(
    variable.name = 'species',
    na.rm = TRUE)
 
-# Data selection ----
-## focusing on plots of 16, 25 and 100 m2 ----
-ddata <- ddata[alpha_grain %in% c(16, 25, 100)]
+## preparing data ----
+ddata[, regional := factor(paste('Jeseniky, CZ,', alpha_grain))
+][, date := data.table::as.IDate(base::strptime(date, "%Y%m%d"))
+][, ":="(
+   year = data.table::year(date),
+   month = data.table::month(date),
+   day = data.table::mday(date)
+)]
 
-## focusing on sites resurveyed at least 10 years apart ----
-ddata[, regional := factor(paste('Jeseniky, CZ,', alpha_grain)),][, year := as.integer(date / 10 ^ 4)]
-ddata <- ddata[ddata[, diff(range(year)), by = .(regional, local)][V1 >= 9L][, V1 := NULL], on = .(regional, local)]
 
-
-# Communities ----
+## Communities ----
 ddata[, ':='(
    dataset_id = dataset_id,
 
    local = factor(base::substr(local, 9L, 11L)),
 
-   metric = 'pa',
-   unit = 'pa',
-   value = 1L,
+   metric = 'cover',
+   unit = 'percent',
 
    species = base::gsub("_[0-9]$", '', species, perl = TRUE),
 
@@ -43,13 +43,11 @@ ddata[, ':='(
 )]
 
 
-# Metadata ----
-meta <- unique(ddata[, .(dataset_id, regional, local, year, latitude, longitude, alpha_grain)])
+## Metadata ----
+meta <- unique(ddata[, .(dataset_id, regional, local, year, month, day, latitude, longitude, alpha_grain)])
 meta[, ':='(
    taxon = "Plants",
    realm = "Terrestrial",
-
-   effort = 1L,
 
    study_type = "resurvey",
    data_pooled_by_authors = FALSE,
@@ -57,6 +55,53 @@ meta[, ':='(
    alpha_grain_unit = "m2",
    alpha_grain_type = "plot",
    alpha_grain_comment = "given by the authors",
+
+      comment = "Extracted from Zenodo respository https://doi.org/10.5281/zenodo.7338814 . 'The data contain plant species composition from vegetation plots repeatedly surveyed in the Hrubý Jeseník Mountains (Eastern Sudetes, Czech Republic). Vegetation plots surveyed by Leoš Bureš and Zuzana Burešová in 1973–1978 were resurveyed in 2004–2010 by Martin Kočí and Leo Bureš and resurveyed again in 2021 by the authors of this dataset. Several new plots were also surveyed in 2004–2010 and resurveyed in 2021'. METHOD: 'Plot size and shape varied by vegetation type. Repeated sampling always used the same plot size as the previous sampling. Most plots were squares of 100 m2 in woodlands and 16 m2 in grasslands or rectangles of 10 m2 in springs. The size/type of plot is given in the alpha_grain column. All species of vascular plants were recorded in each plot'. ",
+   comment_standardisation = "none needed",
+   doi = 'https://doi.org/10.1111/avsc.12711'
+)]
+
+ddata[, c("latitude", "longitude") := NULL]
+
+## saving raw data ----
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(
+   x = ddata[,!"alpha_grain"],
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+   row.names = FALSE)
+data.table::fwrite(
+   x = meta,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+   row.names = FALSE)
+
+
+# Standardised data ----
+## Communities ----
+
+### Data selection ----
+#### focusing on plots of 16, 25 and 100 m2 ----
+ddata <- ddata[alpha_grain %in% c(16, 25, 100)][, alpha_grain := NULL]
+
+#### focusing on sites resurveyed at least 10 years apart ----
+ddata <- ddata[
+   ddata[, diff(range(year)), by = .(regional, local)][V1 >= 9L][, V1 := NULL],
+   on = .(regional, local)]
+
+ddata[, ":="(
+   value = 1L,
+   metric = 'pa',
+   unit = 'pa',
+
+   month = NULL,
+   day = NULL
+)]
+
+## Metadata ----
+meta <- meta[unique(ddata[, .(regional, local, year)]),
+             on = .(regional, local, year)]
+
+meta[, ":="(
+   effort = 1L,
 
    gamma_sum_grains_unit = "m2",
    gamma_sum_grains_type = "plot",
@@ -66,21 +111,21 @@ meta[, ':='(
    gamma_bounding_box_type = "convex-hull",
    gamma_bounding_box_comment = "coordinates provided by the authors",
 
-   comment = "Extracted from Zenodo respository https://doi.org/10.5281/zenodo.7338814 . 'The data contain plant species composition from vegetation plots repeatedly surveyed in the Hrubý Jeseník Mountains (Eastern Sudetes, Czech Republic). Vegetation plots surveyed by Leoš Bureš and Zuzana Burešová in 1973–1978 were resurveyed in 2004–2010 by Martin Kočí and Leo Bureš and resurveyed again in 2021 by the authors of this dataset. Several new plots were also surveyed in 2004–2010 and resurveyed in 2021'. METHOD: 'Plot size and shape varied by vegetation type. Repeated sampling always used the same plot size as the previous sampling. Most plots were squares of 100 m2 in woodlands and 16 m2 in grasslands or rectangles of 10 m2 in springs. All species of vascular plants were recorded in each plot'. ",
    comment_standardisation = "Only plots of 16, 25 and 100 square meters were kept. The data set was split between these three plot sizes in three regions: Jeseniky, CZ, 16 ; Jeseniky, CZ, 25 and Jeseniky, CZ, 100. Only sites sampled at least 10 years apart were kept. Percentage cover was converted into presence absence.",
-   doi = 'https://doi.org/10.1111/avsc.12711'
-)][, ":="(gamma_bounding_box = geosphere::areaPolygon(data.frame(longitude, latitude)[grDevices::chull(longitude, latitude), ]) / 10^6,
-          gamma_sum_grains = sum(alpha_grain)),
-   by = .(regional, year)]
 
-ddata[, c("alpha_grain", "latitude", "longitude") := NULL]
+   month = NULL,
+   day = NULL
+)][, ":="(
+   gamma_bounding_box = geosphere::areaPolygon(data.frame(longitude, latitude)[grDevices::chull(longitude, latitude), ]) / 10^6,
+           gamma_sum_grains = sum(alpha_grain)),
+    by = .(regional, year)]
 
-# Saving ----
-dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
-data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
-                   row.names = FALSE
-)
-
-data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
-                   row.names = FALSE
-)
+## saving standardised data ----
+data.table::fwrite(
+   x = ddata,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised.csv"),
+   row.names = FALSE)
+data.table::fwrite(
+   x = meta,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised_metadata.csv"),
+   row.names = FALSE)
