4ab50eac8dd6ad0f1d810e3052db1f7be1a232c6
AlbanSagouis <alban.sagouis@idiv.de>
2023-08-02 12:34:17 +0200

    splitting kolasa_2022

--- R/data wrangling/kolasa_2022.R
+++ R/data wrangling/kolasa_2022.R
@@ -2,15 +2,12 @@
 dataset_id <- 'kolasa_2022'
 
 ddata <- base::readRDS(file = 'data/raw data/kolasa_2022/rdata.rds')
-data.table::setnames(ddata, c('pool_id', 'latin_name', 'otu_absolute_abundance'), c('local', 'species', 'value'))
+data.table::setnames(x = ddata,
+                     old = c('pool_id', 'latin_name', 'otu_absolute_abundance'),
+                     new = c('local', 'species', 'value'))
 
-# Selecting data ----
-ddata <- ddata[
-   ddata[month != 6L][, .(keep = length(unique(month)) == 1L, month = unique(month)), by = .(local, year)][(keep) | month == 1L][, keep := NULL],
-   on = .(year, month, local)
-]
-
-# Communities ----
+# Standardised data ----
+## Communities ----
 ddata[, ':='(
    dataset_id = dataset_id,
 
@@ -21,12 +18,11 @@ ddata[, ':='(
    metric = 'abundance',
    unit = 'count',
 
-   otu_id = NULL,
-   month = NULL
+   otu_id = NULL
 )]
 
-# Metadata ----
-meta <- unique(ddata[, .(dataset_id, regional, local, year)])
+## Metadata ----
+meta <- unique(ddata[, .(dataset_id, regional, local, year, month)])
 
 meta[, ':='(
    taxon = "Invertebrates",
@@ -45,6 +41,40 @@ meta[, ':='(
    alpha_grain_type = "lake_pond",
    alpha_grain_comment = "mean area of the pools, mean diameter given by the authors",
 
+   comment = 'Data extracted from:  Schenk, Siobhan, Lavender, Thomas Michael, and Kolasa, Jurek. 2023. "Long-Term Supratidal Rockpool Invertebrate Community, Discovery Bay, Jamaica." Ecology e4013. https://doi.org/10.1002/ecy.4013 . METHOD: "A sample of rockpool invertebrates is a collection of individuals of all species extracted from 500 mL of homogenized rockpool contents" Macro- and microinvertebrates were then counted in the lab.',
+   comment_standardisation = 'Redundant rows were deleted.',
+   doi = 'https://doi.org/10.1002/ecy.4013 | https://doi.org/10.5683/SP3/FNAU9L | https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.3078'
+)]
+
+## Saving raw data ----
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(
+   x = ddata,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+   row.names = FALSE
+)
+
+data.table::fwrite(
+   x = meta,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+   row.names = FALSE
+)
+
+# Standardised data ----
+## Selecting data ----
+ddata <- ddata[
+   ddata[month != 6L][, .(keep = length(unique(month)) == 1L, month = unique(month)), by = .(local, year)][(keep) | month == 1L][, keep := NULL],
+   on = .(year, month, local)
+][, month := NULL]
+
+## Metadata ----
+meta <- meta[unique(ddata[, .(local, year)]),
+             on = .(local, year)]
+
+meta[, ":="(
+
+   effort = 1L,
+
    gamma_sum_grains_unit = "cm2",
    gamma_sum_grains_type = "lake_pond",
    gamma_sum_grains_comment = "sum of the sampled areas from all pools on a given year",
@@ -54,17 +84,19 @@ meta[, ':='(
    gamma_bounding_box_type = "box",
    gamma_bounding_box_comment = "box encompassing all pools, given by the authors",
 
-   comment = 'Data extracted from:  Schenk, Siobhan, Lavender, Thomas Michael, and Kolasa, Jurek. 2023. "Long-Term Supratidal Rockpool Invertebrate Community, Discovery Bay, Jamaica." Ecology e4013. https://doi.org/10.1002/ecy.4013 . METHOD: "A sample of rockpool invertebrates is a collection of individuals of all species extracted from 500 mL of homogenized rockpool contents" Macro- and microinvertebrates were then counted in the lab.',
    comment_standardisation = 'Redundant rows were deleted. June samples were excluded. When a pool was sampled twice during a year (January and December), only January was kept.',
-   doi = 'https://doi.org/10.1002/ecy.4013 | https://doi.org/10.5683/SP3/FNAU9L | https://esajournals.onlinelibrary.wiley.com/doi/full/10.1002/ecs2.3078'
+   month = NULL
 )][, gamma_sum_grains := sum(alpha_grain), by = year]
 
-# Saving ----
-dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
-data.table::fwrite(ddata, paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
-                   row.names = FALSE
+## Saving standardised data ----
+data.table::fwrite(
+   x = ddata,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised.csv"),
+   row.names = FALSE
 )
 
-data.table::fwrite(meta, paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
-                   row.names = FALSE
+data.table::fwrite(
+   x = unique(meta),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised_metadata.csv"),
+   row.names = FALSE
 )
