20ea577b95ede8e743697f31c4093364551569c6
AlbanSagouis <alban.sagouis@idiv.de>
2023-08-02 15:14:59 +0200

    splitting macdonald_2023 trees & understory

--- R/data wrangling/macdonald_2023_trees.R
+++ R/data wrangling/macdonald_2023_trees.R
@@ -2,7 +2,8 @@ dataset_id <- 'macdonald_2023_trees'
 
 ddata <- base::readRDS('data/raw data/macdonald_2023_trees/rdata.rds')
 
-# Spatial data ----
+# Data preparation ----
+## Spatial data ----
 coords <- data.frame(matrix(ncol = 3, byrow = TRUE, data = c(
    'Athabasca',   -117.9696549,  52.75510395,
    'Hector',      -116.2567177,  51.50370852,
@@ -12,7 +13,7 @@ coords <- data.frame(matrix(ncol = 3, byrow = TRUE, data = c(
    dimnames = list(c(), c('regional', 'longitude', 'latitude'))
 ))
 
-# melting species ----
+## melting species ----
 ddata <- base::suppressWarnings(
    data.table::melt(
       data = ddata,
@@ -20,13 +21,16 @@ ddata <- base::suppressWarnings(
       variable.name = 'species', na.rm = TRUE)
 )
 
-data.table::setnames(x = ddata, c('Site','Q-1967','Year_sampled'), c('regional','local','year'))
+data.table::setnames(x = ddata,
+                     old = c('Site','Q-1967','Year_sampled'),
+                     new = c('regional','local','year'))
 
-# Standardising data ----
+# Raw data ----
+
+## Community data ----
 # For each sample and species, we sum abundances from saplings and the different size classes
 ddata <- ddata[, species := base::substr(species, 1, 2)][, .(value = sum(value)), by = .(regional, local, year, species)]
 
-# Community data ----
 ddata[, ":="(
    dataset_id = dataset_id,
 
@@ -34,14 +38,15 @@ ddata[, ":="(
    unit = 'count'
 )]
 
-# metadata ----
+## metadata ----
 meta <- unique(ddata[, .(dataset_id, regional, local, year)])
-meta <- meta[coords, on = 'regional']
+meta[coords,
+     ":="(latitude = i.latitude, longitude = i.longitude),
+     on = 'regional']
+
 meta[, ":="(
    taxon = "Plants",
    realm = "Terrestrial",
-
-   effort = 1L,
 
    study_type = "ecological_sampling",
 
@@ -52,6 +57,32 @@ meta[, ":="(
    alpha_grain_type = "quadrat",
    alpha_grain_comment = "area of a 5m*5m plot",
 
+   comment = "Data were found in file Tree_data_1967_1989_2012_quadrat_level.csv manually downloaded from the Borealis repository https://doi.org/10.5683/SP3/YAQCWD. The authors sampled trees and undestory vegetation from 5 1ha sites devided in 400 quadrats. Here we focus on trees only. Site coordinates given in 1b_ReadMe_Summary.txt",
+   comment_standardisation = "Size classes were pooled together.",
+   doi = 'https://doi.org/10.5683/SP3/YAQCWD'
+)]
+
+
+## Saving raw data ----
+dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+data.table::fwrite(
+   x = ddata,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw.csv"),
+   row.names = FALSE
+)
+data.table::fwrite(
+   x = meta,
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_raw_metadata.csv"),
+   row.names = FALSE
+)
+
+
+# Standardised data ----
+
+## Metadata ----
+meta[, ":="(
+   effort = 1L,
+
    gamma_sum_grains = 5L * 5L * 397L,
    gamma_sum_grains_unit = "m2",
    gamma_sum_grains_type = "quadrat",
@@ -60,21 +91,17 @@ meta[, ":="(
    gamma_bounding_box = 1L,
    gamma_bounding_box_unit = "ha",
    gamma_bounding_box_type = "plot",
-   gamma_bounding_box_comment = "each region is a 1ha permanent plot split into 400 5*5m quadrats",
-
-   comment = "Data were found in file Tree_data_1967_1989_2012_quadrat_level.csv manually downloaded from the Borealis repository https://doi.org/10.5683/SP3/YAQCWD. The authors sampled trees and undestory vegetation from 5 1ha sites devided in 400 quadrats. Here we focus on trees only. Site coordinates given in 1b_ReadMe_Summary.txt",
-   comment_standardisation = "cover of dead trees was excluded. Size classes were pooled together.",
-   doi = 'https://doi.org/10.5683/SP3/YAQCWD'
+   gamma_bounding_box_comment = "each region is a 1ha permanent plot split into 400 5*5m quadrats"
 )]
 
-dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+## Saving standardised data ----
 data.table::fwrite(
    x = ddata,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, ".csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised.csv"),
    row.names = FALSE
 )
 data.table::fwrite(
    x = meta,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_metadata.csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised_metadata.csv"),
    row.names = FALSE
 )
