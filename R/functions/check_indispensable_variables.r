8f115d3461b8fdf6da9d0652227f417450e30561
AlbanSagouis <alban.sagouis@idiv.de>
2023-08-04 12:46:37 +0200

    fix function for factors

--- R/functions/check_indispensable_variables.r
+++ R/functions/check_indispensable_variables.r
@@ -3,12 +3,12 @@
 # indispensable_variables is a character vector of column names where no NA is allowed
 
 check_indispensable_variables <- function(dt, indispensable_variables) {
-   na_variables <- apply(dt[, ..indispensable_variables], 2, function(variable) any(is.na(variable)))
+   na_variables <- apply(dt[, ..indispensable_variables], 2, anyNA)
    if (any(na_variables)) {
       na_variables_names <- indispensable_variables[na_variables]
 
       for (na_variable in na_variables_names) {
-         warning(paste0("The variable -", na_variable, "- has missing values in the following datasets: ", paste(as.character(unique(dt[c(is.na(dt[, ..na_variable])), "dataset_id"])), collapse = ", ")))
+         warning(paste0("The variable -", na_variable, "- has missing values in the following datasets: ", paste(as.character(unlist(unique(dt[c(is.na(dt[, ..na_variable])), "dataset_id"]))), collapse = ", ")))
       }
    }
 }
