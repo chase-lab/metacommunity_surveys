0b2f184f4066a815f433fe4d6c3b0df67ede1f05
AlbanSagouis <alban.sagouis@idiv.de>
2023-08-18 16:03:11 +0200

    splitting and fixing quimbayo_2022

--- R/data wrangling/quimbayo_2022.R
+++ R/data wrangling/quimbayo_2022.R
@@ -11,15 +11,20 @@ data.table::setnames(ddata,
 ddata[, ":="(
    dataset_id = dataset_id,
 
+   local = factor(paste(local, site, sep = "_")),
+
    metric = "abundance",
    unit = "count",
 
+   month = c(1L:5L, 7L:12L)[base::match(
+      x = month,
+      table = c("january", "february", "march", "april", "may", "july", "august",
+                "september", "october", "november", "december"))],
    site = NULL
 )]
 
 ## metadata ----
-
-meta <- unique(ddata[, .(dataset_id, regional, local, year, latitude, longitude)])
+meta <- unique(ddata[, .(dataset_id, regional, local, year, month, day, latitude, longitude)])
 meta[, ":="(
    realm = "Marine",
    taxon = "Fish",
@@ -36,10 +41,7 @@ meta[, ":="(
    comment = "Extracted from supplementary material associated to data paper https://doi.org/10.1002/ecy.3966. TimeFISH is a database of reef fish communities in Southwest Atlantic. Methods: 'A total of 202,965 individuals belonging to 163 reef fish species and 53 families were recorded across 1,857 [Under Water Visual Censuses]'. One 40m2 standard sample per year. Both 'scuba' and 'snorkel' samples were included. Authors provided three scales: location, site and transect ; here, location is regional, transect number is local and the site scale is ignored.",
    comment_standardisation = "none needed",
    doi = 'https://doi.org/10.1002/ecy.3966'
-)
-][, ":="(gamma_bounding_box = geosphere::areaPolygon(data.frame(longitude, latitude)[grDevices::chull(longitude, latitude), ]) / 10^6,
-         gamma_sum_grains = sum(alpha_grain)),
-  by = .(regional, year)]
+)]
 
 ddata[, c("latitude", "longitude") := NULL]
 
@@ -59,11 +61,15 @@ data.table::fwrite(
 # Standardised data ----
 ## Standardisation ----
 # "transect_ids 2019_86 and 2019_91 were excluded over uncertainty whether data are duplicated or not."
-ddata <- ddata[!transect_id %in% c("2019_86", "2019_91")]
+ddata <- ddata[!transect_id %in% c("2019_86", "2019_91")][, transect_id := NULL]
 # regions with less than 4 sampled locations per year are excluded (Campeche island2013 and 2017)
-ddata <- ddata[ddata[, length(unique(local)), by = regional][V1 >= 4L][, V1 := NULL], on = .(regional)]
+ddata <- ddata[ddata[, data.table::uniqueN(local),
+                     by = regional][V1 >= 4L][, V1 := NULL],
+               on = .(regional)]
 
 ## Metadata ----
+meta[, c("month", "day") := NULL]
+meta <- unique(meta)
 meta <- meta[unique(ddata[, .(regional, local, year)]),
              on = .(regional, local, year)]
 
@@ -79,7 +85,9 @@ meta[, ":="(
    gamma_bounding_box_comment = "coordinates provided by the authors",
 
    comment_standardisation = "transect_ids 2019_86 and 2019_91 were excluded over uncertainty whether data are duplicated or not."
-)]
+)][, ":="(gamma_bounding_box = geosphere::areaPolygon(data.frame(longitude, latitude)[grDevices::chull(longitude, latitude), ]) / 10^6,
+          gamma_sum_grains = sum(alpha_grain)),
+   by = .(regional, year)]
 
 ## Saving standardised data ----
 data.table::fwrite(
