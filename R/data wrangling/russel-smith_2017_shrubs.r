f6a88246ba0ab65e2c7b52dfa70ec7367c4aafb1
AlbanSagouis <alban.sagouis@idiv.de>
2023-08-21 11:44:51 +0200

    splitting and fixing russel-smith trees and shrubs

--- R/data wrangling/russel-smith_2017_shrubs.r
+++ R/data wrangling/russel-smith_2017_shrubs.r
@@ -7,7 +7,7 @@ dataset_id <- "russell-smith_2017_shrubs"
 ###  https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5837/data/
 ###  Login for Australian National University needed. Data accessible after login without further requests.
 
-# loading data ----
+#  Data preparation ----
 datafiles <- c(
    "./data/raw data/russell-smith_2017/data/tpsk_shrubs_1994+_p831t1065.csv",
    "./data/raw data/russell-smith_2017/data/tpsl_shrubs_1994+_p831t1123.csv",
@@ -53,16 +53,9 @@ spatial <- data.table::rbindlist(
    ),
    use.names = TRUE, idcol = FALSE, fill = TRUE
 )
-#Raw Data ----
-## cleaning: deleting duplicated rows ----
-duplicated_subsets <- unique(ddata[duplicated(ddata), .(park, plot, visit)])
-ddata <- ddata[!duplicated_subsets, on = c("park", "plot", "visit")]
 
 ## merging data.table style ----
 ddata <- dates[ddata, on = c("park", "plot", "visit")]
-# unique(ddata[is.na(as.integer(`count_50cm-2m`))]$`count_50cm-2m`)
-# unique(ddata[is.na(as.integer(count_less_than_50cm))]$count_less_than_50cm)
-# unique(ddata[is.na(as.integer(count_greater_than_2m))]$count_greater_than_2m)
 
 ## Summing abundances from different size class ----
 ddata[, ":="(
@@ -71,27 +64,34 @@ ddata[, ":="(
    count_greater_than_2m = as.integer(count_greater_than_2m)
 )]
 
-ddata[, value := apply(ddata[, .(count_less_than_50cm, `count_50cm-2m`, count_greater_than_2m)], 1, sum, na.rm = TRUE)]
+ddata[, value := base::rowSums(.SD, na.rm = TRUE), .SDcols = c("count_less_than_50cm", "count_50cm-2m", "count_greater_than_2m")]
+
 ## remove absences
 ddata <- ddata[value != 0L]
 
-data.table::setnames(ddata, c("park", "plot","genus_species"), c("regional","local","species"))
+data.table::setnames(ddata,
+                     old = c("park", "plot","genus_species"),
+                     new = c("regional","local","species"))
 
 #format spatial data to have common identifier with ddata
 spatial[, regional := c("Kakadu", "Litchfield", "Nitmiluk")[data.table::chmatch(substr(plot, 1, 3), c("KAK", "LIT", "NIT"))]]
-spatial[, local := stringi::stri_extract_all_regex(str = plot, pattern = "[0-9]{2,3}")][, local := as.integer(sub("^0+(?=[1-9])", "", local, perl = TRUE))]
+spatial[, local := stringi::stri_extract_all_regex(str = plot, pattern = "[0-9]{2,3}")
+        ][, local := as.integer(sub("^0+(?=[1-9])", "", local, perl = TRUE))]
 
 
-# communities ----
+# Raw Data ----
+## communities ----
 
 ddata[, ":="(
    dataset_id = dataset_id,
-   
-   year = format(date, "%Y"),
-   
+
+   year = data.table::year(date),
+   month = data.table::month(date),
+   day = data.table::mday(date),
+
    metric = "abundance",
    unit = "count",
-   
+
    visit = NULL,
    date = NULL,
    comments = NULL,
@@ -103,31 +103,39 @@ ddata[, ":="(
 ##remove NA values in year because of missing dates in original data ----
 ddata <- ddata[!is.na(year)]
 
+## cleaning: deleting samples with duplicated rows ----
+ddata <- ddata[
+   !unique(ddata[, .N, by = .(regional, local, year, month, day, species)][N != 1L]),
+   on = c("regional", "local", "year", "month", "day")
+   ]
 
-meta <- unique(ddata[, .(dataset_id, year, regional, local)])
-meta <- spatial[, .(local, regional, latitude, longitude)][meta, on = c("local", "regional")]
+## Metadata ----
+meta <- unique(ddata[, .(dataset_id, regional, local, year, month, day)])
+meta <- meta[spatial[, .(local, regional, latitude, longitude)],
+     on = .(regional, local), nomatch = NULL]
+
 meta[, ":="(
    realm = "Terrestrial",
    taxon = "Plants",
-   
+
    study_type = "ecological_sampling", #two possible values, or NA if not sure
-   
+
    data_pooled_by_authors = FALSE,
    data_pooled_by_authors_comment = NA,
    sampling_years = NA,
-   
+
    alpha_grain = 400L,  #area of individual plot
    alpha_grain_unit = "m2", #"acres", "ha", "km2", "m2", "cm2"
    alpha_grain_type = "plot",
    alpha_grain_comment = "all shrubs counted in 40m *10m plots",
-   
-   comment = "Data manually downloaded via https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5836/data/ with login for national university of australia website. Authors sampled shrubs from the inner 40m*10m plot of their fixed 40m*20m plots once a year. They measured the height of all shrubs and we kept only abundances.",
-   comment_standardisation = "some visit numbers (T1, T2, ...) had no match (year) in the dates table so they were excluded. Authors sampled shrubs from the smallest size class in subplots but since the size of subplot is constant, no standardisation is needed. Some rows were duplicated so all results from these 5 problematic plot/year subsets were excluded. Sites sampled only one year were excluded.",
+
+   comment = factor("Data manually downloaded via https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5836/data/ with login for national university of australia website. Authors sampled shrubs from the inner 40m*10m plot of their fixed 40m*20m plots once a year. They measured the height of all shrubs and we kept only abundances."),
+   comment_standardisation = factor("some visit numbers (T1, T2, ...) had no match (year) in the dates table so they were excluded.
+Some rows were duplicated so all results from these problematic plot/year subsets were excluded."),
    doi = 'https://doi.org/10.25911/5c3d75bbca1c0'
 )]
 
-
-##save data ----
+## save raw data ----
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
 data.table::fwrite(
    x = ddata,
@@ -140,39 +148,49 @@ data.table::fwrite(
    row.names = FALSE
 )
 
-#Standardization ----
+# standardised data ----
 ## deleting site sampled only once with a data.table style join ----
-ddata <- ddata[ ddata[, .(n_years = length(unique(year))), by = .(regional, local)][n_years > 1L][, .(regional, local)], on = .(regional, local)]
+ddata <- ddata[
+   ddata[,
+         .(n_years = data.table::uniqueN(year)),
+         by = .(regional, local)][n_years != 1L][, .(regional, local)],
+   on = .(regional, local)]
 
-##meta data ----
-meta <- meta[unique(ddata[, .(dataset_id, local, regional, year)]),
-             on = .(local, regional, year)]
+## Only one sample per year
+# ddata[, data.table::uniqueN(.SD), by = .(regional, local, year), .SDcols = c("month", "day")][, any(V1 != 1L)]
+ddata[, c("month","day") := NULL]
+
+## meta data ----
+meta <- meta[unique(ddata[, .(regional, local, year)]),
+             on = .(regional, local, year)]
 meta[, ":="(
+   month = NULL,
+   day = NULL,
+
    effort = 1L,
-   
+
    gamma_sum_grains_unit = "m2",
    gamma_sum_grains_type = "plot",
    gamma_sum_grains_comment = "area of the sampled plots per year multiplied by amount of plots per region",
-   
+
    gamma_bounding_box_unit = "km2",
    gamma_bounding_box_type = "convex-hull",
    gamma_bounding_box_comment = "convex-hull over the coordinates of sample points",
-   
-   comment_standardisation = "some visit numbers (T1, T2, ...) had no match (year) in the dates table so they were excluded. Authors sampled shrubs from the smallest size class in subplots but since the size of subplot is constant, no standardisation is needed. Some rows were duplicated so all results from these 5 problematic plot/year subsets were excluded. Sites sampled only one year were excluded."
+
+   comment_standardisation = factor("some visit numbers (T1, T2, ...) had no match (year) in the dates table so they were excluded. Authors sampled shrubs from the smallest size class in subplots but since the size of subplot is constant, no standardisation is needed. Some rows were duplicated so all results from these 5 problematic plot/year subsets were excluded. Sites sampled only one year were excluded.")
 )][, ":="(
    gamma_bounding_box = geosphere::areaPolygon(data.frame(na.omit(longitude), na.omit(latitude))[grDevices::chull(na.omit(longitude), na.omit(latitude)), ]) / 10^6,
    gamma_sum_grains = sum(alpha_grain)
 ), by = .(regional, year)]
 
-##save data ----
-dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+## save standardised data ----
 data.table::fwrite(
    x = ddata,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized.csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised.csv"),
    row.names = FALSE
 )
 data.table::fwrite(
    x = meta,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized_metadata.csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised_metadata.csv"),
    row.names = FALSE
 )
--- R/data wrangling/russel-smith_2017_trees.r
+++ R/data wrangling/russel-smith_2017_trees.r
@@ -7,8 +7,8 @@ dataset_id <- "russell-smith_2017_trees"
 ###  https://datacommons.anu.edu.au/russell-smith_2017/dataCommons/rest/records/anudc:5837/data/
 ###  Login for Australian National University needed. Data accessible after login without further requests.
 
-#Raw Data ----
-##Loading data ----
+# Raw Data ----
+## Loading data ----
 datafiles <- c(
    "./data/raw data/russell-smith_2017/data/tpsk_trees_1994+_p831t1066.csv",
    "./data/raw data/russell-smith_2017/data/tpsl_trees_1994+_p831t1124.csv",
@@ -27,85 +27,94 @@ datafiles_spatial <- c(
    "./data/raw data/russell-smith_2017/spatial/tpsn_plot_details_spatial_coordinates_p894t1156.csv"
 )
 
-
-
 ddata <- data.table::rbindlist(
+   lapply(datafiles, data.table::fread),
    fill = TRUE,
-   lapply(
-      datafiles,
-      FUN = function(x)
-         data.table::fread(file = x)
-   ),
    use.names = TRUE, idcol = FALSE
 )
 
 dates <- data.table::rbindlist(
-   lapply(
-      datafiles_dates,
-      FUN = function(x)
-         data.table::fread(file = x)
-   ),
+   lapply(datafiles_dates, data.table::fread),
    use.names = TRUE, idcol = FALSE
 )
 
 spatial <- data.table::rbindlist(
-   lapply(
-      datafiles_spatial,
-      FUN = function(x)
-         data.table::fread(file = x)
-   ),
+   lapply(datafiles_spatial, data.table::fread),
    use.names = TRUE, idcol = FALSE, fill = TRUE
 )
 
-#Raw Data ----
-##merge data and dates ----
-ddata <- dates[ddata, on = c("park", "plot", "visit")]
-ddata <- ddata[,.N, by = .(park, plot, visit, genus_species, date)]
+## data preparation ----
+### merge data and dates ----
+ddata[dates, date := i.date, on = .(park, plot, visit)]
 
-data.table::setnames(ddata, c("park", "plot","genus_species", "N"), c("regional","local","species", "value"))
+### remove NA values in year because of missing dates in original data ----
+ddata[, year := data.table::year(date)]
+ddata <- na.omit(ddata, cols = "year")
 
-##format spatial data to have common identifier with species data ----
+### Sum individual observations to get species abundances ----
+ddata <- ddata[, .N, by = .(park, plot, visit, genus_species, year, date)]
+
+data.table::setnames(ddata,
+                     old = c("park", "plot","genus_species", "N"),
+                     new = c("regional","local","species", "value"))
+
+### excluding unknown species ----
+ddata <- ddata[species != ""]
+
+### format spatial data to have common identifier with species data ----
 spatial[, regional := c("Kakadu","Litchfield","Nitmiluk")[data.table::chmatch(substr(plot, 1, 3), c("KAK", "LIT", "NIT"))]]
 spatial[, local := stringi::stri_extract_all_regex(str = plot, pattern = "[0-9]{2,3}")
 ][, local := as.integer(sub("^0+(?=[1-9])", "", local, perl = TRUE))]
 
-##community data ----
+# Raw Data ----
+## community data ----
 
 ddata[, ":="(
    dataset_id = dataset_id,
-   year = format(date, "%Y"),
-   
+
+   month = data.table::month(date),
+   day = data.table::mday(date),
+
    visit = NULL,
    date = NULL,
-   
+
    metric = "abundance",
    unit = "count"
 )]
 
-meta <- unique(ddata[, .(dataset_id, year, regional, local)])
-meta <- spatial[, .(local, regional, latitude, longitude)][meta, on = c("local", "regional")]
+## cleaning: deleting samples with duplicated rows ----
+ddata <- ddata[
+   !unique(ddata[, .N, by = .(regional, local, year, month, day, species)][N != 1L]),
+   on = c("regional", "local", "year", "month", "day")
+]
+
+meta <- unique(ddata[, .(dataset_id, regional, local, year, month, day)])
+meta[spatial,
+     ":="(latitude = i.latitude, longitude = i.longitude),
+     on = .(regional, local)]
 
 meta[, ":="(
    realm = "Terrestrial",
    taxon = "Plants",
-   
+
    study_type = "ecological_sampling", #two possible values, or NA if not sure
-   
+
    data_pooled_by_authors = FALSE,
    data_pooled_by_authors_comment = NA,
    sampling_years = NA,
-   
+
    alpha_grain = 800L,  #area of individual plot
    alpha_grain_unit = "m2", #"acres", "ha", "km2", "m2", "cm2"
    alpha_grain_type = "plot",
    alpha_grain_comment = "all trees defined as wooden species with diameter at breast hight > 5cm are counted in 40*20m plot ",
-   
-   comment = "Data manually downloaded via https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5836/data/ with login for national university of australia webpage. Authors sampled trees with DBH (diameter at breast hight) > 5cm in fixed 40m*20m plots once a year.",
-   comment_standardisation = "None needed",
+
+   comment = factor("Data manually downloaded via https://datacommons.anu.edu.au/DataCommons/rest/records/anudc:5836/data/ with login for national university of australia webpage. Authors sampled trees with DBH (diameter at breast hight) > 5cm in fixed 40m*20m plots once a year."),
+   comment_standardisation = "some visit numbers (T1, T2,...) have no match (year) in the dates table so they were excluded.
+Some rows were duplicated so all results from these problematic plot/year subsets were excluded.",
    doi = 'https://doi.org/10.25911/5c3d75bbca1c0'
 )]
 
-##save data ----
+## save data ----
 dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
 data.table::fwrite(
    x = ddata,
@@ -118,48 +127,50 @@ data.table::fwrite(
    row.names = FALSE
 )
 
-#Standardized Data -----
+# standardised Data -----
+## deleting site sampled only once with a data.table style join ----
+ddata <- ddata[
+   ddata[,
+         .(n_years = length(unique(year))),
+         by = .(regional, local)][n_years > 1L][, .(regional, local)],
+   on = .(regional, local)]
 
-##excluding unknown species ----
-ddata <- ddata[species != ""]
+## Only one sample per year
+# ddata[, data.table::uniqueN(.SD), by = .(regional, local, year), .SDcols = c("month", "day")][, any(V1 != 1L)]
+ddata[, c("month","day") := NULL]
 
-##remove NA values in year because of missing dates in original data ----
-ddata <- na.omit(ddata, cols = "year")
-
-##deleting site sampled only once with a data.table style join ----
-ddata <- ddata[ ddata[, .(n_years = length(unique(year))), by = .(regional, local)][n_years > 1L][, .(regional, local)], on = .(regional, local)]
-
-
-##meta data ----
-meta <- meta[unique(ddata[, .(dataset_id, local, regional, year)]),
+## meta data ----
+meta[, c("month", "day") := NULL]
+meta <- meta[unique(ddata[, .(local, regional, year)]),
              on = .(local, regional, year)]
 
 meta[, ":="(
    effort = 1L,
-   
+
    gamma_sum_grains_unit = "m2",
    gamma_sum_grains_type = "plot",
    gamma_sum_grains_comment = "area of the sampled plots per year multiplied by amount of plots per region",
-   
+
    gamma_bounding_box_unit = "km2",
    gamma_bounding_box_type = "convex-hull",
    gamma_bounding_box_comment = "convex-hull over the coordinates of sample points",
-   
-   comment_standardisation = "some visit numbers (T1, T2,...) have no match (year) in the dates table so they were excluded. Sites sampled only one year were excluded."
+
+   comment_standardisation = "some visit numbers (T1, T2,...) have no match (year) in the dates table so they were excluded.
+Some rows were duplicated so all results from these problematic plot/year subsets were excluded.
+Sites sampled only one year were excluded."
 )][, ":="(
    gamma_bounding_box = geosphere::areaPolygon(data.frame(na.omit(longitude), na.omit(latitude))[grDevices::chull(na.omit(longitude), na.omit(latitude)), ]) / 10^6,
    gamma_sum_grains = sum(alpha_grain)
 ),by = .(regional, year)]
 
-##save data ----
-dir.create(paste0("data/wrangled data/", dataset_id), showWarnings = FALSE)
+## save standardised data ----
 data.table::fwrite(
    x = ddata,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized.csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised.csv"),
    row.names = FALSE
 )
 data.table::fwrite(
    x = meta,
-   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardized_metadata.csv"),
+   file = paste0("data/wrangled data/", dataset_id, "/", dataset_id, "_standardised_metadata.csv"),
    row.names = FALSE
 )
